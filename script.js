50a1f533afe9530b76ed02b4c49a738233872ce8
Shivansh <shivanshgarg2005@gmail.com>
2024-09-27 14:03:49 +0530

    Adding Registration

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
-const db = require('./db');
+let lusername = localStorage.getItem('usernames');
+    let lpassword = localStorage.getItem('password');
+    
+    lusername = lusername ? lusername.split(",") : []
+    lpassword = lpassword ? lpassword.split(",") : []
 
-// Function to add a new user
-const addUser = (name, email, password) => {
-  const sql = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
-  db.query(sql, [name, email, password], (err, result) => {
-    if (err) {
-      console.error('Error inserting user:', err);
-    } else {
-      console.log('User added with ID:', result.insertId);
+    function login() {
+        let counter = 0;
+        let username = document.querySelector('#email').value;
+        let pass = document.querySelector('#password').value;
+        if (!username || !pass) {
+            alert("Please Enter username and password")
+        }
+
+        for (let i = 0; i < lusername.length; i++) {
+            if (username === lusername[i] && pass === lpassword[i]) {
+                counter++;
+            }
+        }
+        if (counter == 1) {
+            alert("Login Granted!")
+        } else {
+            alert("Login Failed!!")
+        }
     }
-  });
-};
+    function register() {
 
-// Example usage
-addUser('John Doe', 'john@example.com', 'password123');
+        let username = document.querySelector('#email').value;
+        let pass = document.querySelector('#password').value;
+        let cpass = document.querySelector('#confirm-password').value;
+        if (!username || !pass || !cpass) {
+            alert("Please enter username or password or confirm-password")
+        }
+        if (pass === cpass) {
+            lusername.push(username);
+            lpassword.push(pass);
+            localStorage.setItem('usernames', lusername);
+            localStorage.setItem('password', lpassword);
+            alert("User Saved !!!!!!!");
+        } else {
+            alert("Retry");
+            document.querySelector('#email').value = "";
+            document.querySelector('#password').value = "";
+            document.querySelector('#confirm-password').value = "";
+        }
+    }
