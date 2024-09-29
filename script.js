bfdc0407c3d99ce4c2d59fe59dbcde9940a3429b
Shivansh <shivanshgarg2005@gmail.com>
2024-09-30 01:30:26 +0530

    What is Roamies

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-let lusername = localStorage.getItem('usernames');
+    let lusername = localStorage.getItem('usernames');
     let lpassword = localStorage.getItem('password');
     
     lusername = lusername ? lusername.split(",") : []
     lpassword = lpassword ? lpassword.split(",") : []
 
+    
     function login() {
         let counter = 0;
         let username = document.querySelector('#email').value;
         let pass = document.querySelector('#password').value;
         if (!username || !pass) {
-            alert("Please Enter username and password")
+            alert("Please Enter username and password");
+            return;
         }
 
         for (let i = 0; i < lusername.length; i++) {
@@ -18,18 +20,20 @@ let lusername = localStorage.getItem('usernames');
             }
         }
         if (counter == 1) {
-            alert("Login Granted!")
+            alert("Login successful!");
+            document.querySelector('#modalId').style.display='none';
         } else {
-            alert("Login Failed!!")
+            alert("Login Failed! Incorrect Password");
         }
     }
     function register() {
 
-        let username = document.querySelector('#email').value;
-        let pass = document.querySelector('#password').value;
-        let cpass = document.querySelector('#confirm-password').value;
+        let username = document.querySelector('#username').value;
+        let pass = document.querySelector('#passwrd').value;
+        let cpass = document.querySelector('#confirm-passwrd').value;
         if (!username || !pass || !cpass) {
-            alert("Please enter username or password or confirm-password")
+            alert("Please enter username or password or confirm-password");
+            return;
         }
         if (pass === cpass) {
             lusername.push(username);
@@ -38,9 +42,10 @@ let lusername = localStorage.getItem('usernames');
             localStorage.setItem('password', lpassword);
             alert("User Saved !!!!!!!");
         } else {
-            alert("Retry");
-            document.querySelector('#email').value = "";
-            document.querySelector('#password').value = "";
-            document.querySelector('#confirm-password').value = "";
+            alert("Passwords do not match. Retry.");
+            document.querySelector('#username').value = "";
+            document.querySelector('#passwrd').value = "";
+            document.querySelector('#confirm-passwrd').value = "";
         }
     }
+    
