50eb1d1da6bc02669c5bf6e99f8060f8a261f30e
Shivansh <shivanshgarg2005@gmail.com>
2024-10-24 16:36:28 +0530

    Hello

---
+++ roamies.js
@@ -0,0 +1,23 @@
+const AsiaBtn = document.getElementById('AsiaBtn');
+const AsiaImages = document.getElementById('AsiaImages');
+AsiaImages.classList.toggle('visible');
+AsiaBtn.addEventListener('click', function () {
+    AsiaImages.classList.toggle('visible');
+});
+
+
+const europeBtn = document.getElementById('europeBtn');
+const europeImages = document.getElementById('europeImages');
+
+europeBtn.addEventListener('click', function () {
+    europeImages.classList.toggle('visible');
+});
+
+const SouthAmericaBtn = document.getElementById('SouthAmericaBtn');
+const SouthAmericaImages = document.getElementById('SouthAmericaImages');
+
+SouthAmericaBtn.addEventListener('click', function () {
+    SouthAmericaImages.classList.toggle('visible');
+});
+
+const Accomodation=document.getElementById('')
